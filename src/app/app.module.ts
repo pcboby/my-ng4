1c3ea8c619f6a66a669bc98d972a5e817e43fadf
pcboby <pcboby@126.com>
2017-07-13 17:34:04 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent, SilderComponent, WraperComponent, FooterComponent } from './tpls';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SilderComponent,
+    WraperComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/tpls/index.ts
@@ -0,0 +1,4 @@
+export * from './header/header.component';
+export * from './silder/silder.component';
+export * from './wraper/wraper.component';
+export * from './footer/footer.component';
